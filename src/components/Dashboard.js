7474dd955c07983bcb4d927076ea1c1e7706f9bf
nupurchauhan6 <nupur.chauhan6@gmail.com>
2022-07-19 17:12:51 -0400

    sorted tweets on dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,12 +4,15 @@ import Tweet from './Tweet';
 const Dashboard = () => {
 
     const tweets = useSelector(state => state.tweets);
+    const tweetIds = tweets ? Object.keys(tweets).sort(
+        (a, b) => tweets[b].timestamp - tweets[a].timestamp
+    ) : [];
 
     return (
         <div>
             <h3 className="center">Your Timeline</h3>
             <ul className="dashboard-list">
-                {tweets && Object.keys(tweets).map((id) => (
+                {tweetIds && tweetIds.map((id) => (
                     <li key={id}>
                         <div><Tweet id={id} /></div>
                     </li>
